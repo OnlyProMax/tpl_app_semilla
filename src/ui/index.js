cef180fb7c739732bd25bd688e75932a27f7e4a3
devel@mauriciocap.com <devel@mauriciocap.com>
2020-02-08 19:38:23 -0300

    OK: radio, programas y escuchar uno

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,8 +1,123 @@
 //INFO: punto de entrada de la parte que se ejecuta en la web
 
 //------------------------------------------------------------
+//S: mover a lib
+function fLog(msg,fToCallAfter) { //U: devuelve una funcion, que al llamarla loguea mensaje y los parametros
+	return function (p1,p2) { console.log(msg,p1,p1); }
+}
+
+function fAppGoTo(link) { //U: una funcion para ir a un link que se puede poner en onClick
+	return function () { appGoTo(link); }
+}
+
+//------------------------------------------------------------
 function scr_home(my){ //U: pantalla principal cuando ya te logueaste
-	my.render= function () { return 'Hola PodemosAprender' }	
+	my.render= function () { 
+		return h('div',{},
+			'Hola PodemosAprender!',
+			h(Button,{onClick: () => appGoTo('/radio')},'radio!'),
+		);
+	}
+}
+
+function cmp_audio(my) { //U: un componente para reproducir audio
+	my.render= function cmp_audio_render(props) {
+		//eventos interesates onEnded: fLog("ended"), onLoadedmetadata: fLog("load")
+		//SEE: https://www.w3schools.com/tags/ref_av_dom.asp
+		return h('audio',{controls: true, ... props},
+				h('source',{src: props.src , type: "audio/ogg"})
+		);
+	}
+}
+
+RADIO_URL="https://www.podemosaprender.org/data_radio/"
+RadioIdx= null; //DFLT
+function radioFecth(wantsReload) {
+	if (RadioIdx && !wantsReload) { return new Promise(cb => cb(RadioIdx)) }
+	//A: si ya lo tenia, lo devolvi, sino lo busco
+	return fetch(RADIO_URL)
+		.then(r => r.text())
+		.then(t => { 
+			RadioIdx= {}; 
+			t.match(/<p>[^<]*/g) //A: array con todo desde <p>hasta<
+			.forEach( f => {
+				var parts= f.split("/"); parts.shift(); //A: tiro <p>.
+				if (parts[1].match(/.ogg/)) { return } //A: no quiero los audios de entrada y salida
+				var programa= RadioIdx[parts[1]] || {titulo: parts[1], audios: []};
+				RadioIdx[parts[1]]= programa; //A: seguro lo guarde e inicialice
+				programa.audios.push(RADIO_URL+'/'+parts.join('/'));
+			});
+			return RadioIdx;
+		});
+}
+
+function scr_radio_$programa(my) { //U: escuchar la radio, un programa, radio/miprograma
+	var wantsPlay= false;
+	var audios= null;
+
+	function audioOnLoadedMetadata(e) {
+		wantsPlay= true;
+		//audioDuration: e.target.duration, 
+		//TODO: no puedo cambiar estado en este evento porque vuelve a hacer render, y elemento de audio pierde estado, carga de nuevo, etc.
+	}
+
+	function audioOnEnded() {
+		if (my.state.audioIdx<audios.length-1) {
+			my.setState({audioIdx: my.state.audioIdx+1}); //A: actualizar
+		}
+		else {
+			my.setState({audioDone: true}); //A: actualizar
+		}
+	}
+
+	function volverAEscuchar() {
+		wantsPlay= true;
+		my.setState({audioIdx: 0, audioDone: false});
+	}
+
+	my.componentWillMount= function () {
+		radioFecth().then(() => my.setState({indexLoaded: true})); //A: cargue lista de programas
+	}
+
+	my.render= function (props, state) { 
+		state.audioIdx= state.audioIdx || 0; 
+		programa= props.matches.programa;
+		audios= RadioIdx && RadioIdx[programa] && RadioIdx[programa].audios
+		console.log("Radio programa="+programa+" audios="+audios);
+
+		return h('div',{},
+			h('h1',{},'Radio PodemosAprender'),
+			h('h2',{},programa),
+			state.indexLoaded 
+				? state.audioDone 
+					? h(Button,{onClick: volverAEscuchar},'Volver a escuchar') 
+					: h('div',{},
+							h('h4',{},"(" + state.audioIdx + "/" + audios.length+") "+audios[state.audioIdx]),
+							h(Cmp.audio,{src: audios[state.audioIdx], onEnded: audioOnEnded, onLoadedmetadata: audioOnLoadedMetadata, autoplay: wantsPlay}),
+							h('div',{},
+								h(Button,{onClick: audioOnEnded},'Próximo') 
+							)
+						)
+				: 'Cargando programa '+programa,
+			h(Button,{onClick: fAppGoTo('/radio')},'Volver a la lista')
+		);
+	}
+}
+
+function scr_radio(my) { //U: escuchar la radio, ver programas
+	my.componentWillMount= function () {
+		radioFecth().then(() => my.setState({indexLoaded: true})); //A: cargue lista de programas
+	}
+
+	my.render= function (props, state) { 
+		return h('div',{},
+			h('h1',{},'Radio PodemosAprender'),
+			state.indexLoaded 
+				? Object.keys(RadioIdx).map( k => h('p',{},h('a',{onClick: fAppGoTo('/radio/'+k)},k)) )
+				: 'Cargando programas ...'
+		);
+	}
+
 }
 
 function scr_login(my){ //U: formulario de ingreso 
